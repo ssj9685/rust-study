6b90b231d6015b1417041e4e23e4605d1e30d2f2
shinseokjin <ssj9685@naver.com>
2022-05-04 01:57:30 +0900

    Add macro rules

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,27 +1,30 @@
 use rand::Rng;
-
+macro_rules! get_line {
+    () => {
+        get_line_func("")
+    };
+    ($title: expr) => {
+        get_line_func($title)
+    };
+}
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess. (0<=x<=10)");
-    check_number();
+    let count: i32 = str_to_int(get_line!("Input Guess Number Range"));
+    println!("Please input your guess. (0<=x<={})", count);
+    check_number(count);
 }
 
-fn check_number() -> () {
-    let random_number: i32 = rand::thread_rng().gen_range(0..11);
+fn check_number(count: i32) -> () {
+    let random_number: i32 = rand::thread_rng().gen_range(0..count);
     loop {
-        let guess = get_line();
-        let guess_number: i32 = match guess.trim().parse::<i32>() {
-            Ok(n) => n,
-            Err(_) => -1,
-        };
+        let guess = str_to_int(get_line!());
         println!("You guessed: {}", guess);
-        if guess_number == -1 {
+        if guess == -1 {
             println!("Please Enter the >=0 number");
             continue;
         }
-        if random_number > guess_number {
+        if random_number > guess {
             println!("Bigger");
-        } else if random_number == guess_number {
+        } else if random_number == guess {
             println!("Match");
             break;
         } else {
@@ -30,7 +33,8 @@ fn check_number() -> () {
     }
 }
 
-fn get_line() -> String {
+fn get_line_func(title: &str) -> String {
+    println!("{}", title);
     let empty_str = "";
     //String::new(); empty string
     let mut guess = empty_str.to_owned();
@@ -38,4 +42,11 @@ fn get_line() -> String {
         .read_line(&mut guess)
         .expect("Failed to read line");
     guess
+}
+
+fn str_to_int(str: String) -> i32 {
+    return match str.trim().parse::<i32>() {
+        Ok(n) => n,
+        Err(_) => -1,
+    };
 }
